af80beed54e3321e890d0380c52bf19cf157f5e8
John Feng <johnfengphd@gmail.com>
2025-03-20 16:35:38 -0700

    Update .gitignore and enhance color scheme handling in utils.py
    - Add additional entries to .gitignore for IDE and Python bytecode files
    - Refactor color scheme selection in get_colors() to improve organization and clarity, separating qualitative and sequential schemes

--- utils.py
+++ utils.py
@@ -15,18 +15,9 @@ def find_pulse_start(df: pd.DataFrame, pulse_start_current: float = 1e-7) -> tup
         return 0, 0
 
 def get_colors(n_files, color_scheme):
+    # qualitative color schemes
     if color_scheme == 'Plotly':
         colors = px.colors.qualitative.Plotly
-    elif color_scheme == 'Viridis':
-        colors = px.colors.sequential.Viridis
-    elif color_scheme == 'Plasma':
-        colors = px.colors.sequential.Plasma
-    elif color_scheme == 'Rainbow':
-        colors = px.colors.sequential.Rainbow
-    elif color_scheme == 'Turbo':
-        colors = px.colors.sequential.Turbo
-    elif color_scheme == 'D3':
-        colors = px.colors.qualitative.D3
     elif color_scheme == 'G10':
         colors = px.colors.qualitative.G10
     elif color_scheme == 'T10':
@@ -37,6 +28,17 @@ def get_colors(n_files, color_scheme):
         colors = px.colors.qualitative.Set2
     elif color_scheme == 'Set3':
         colors = px.colors.qualitative.Set3
+    # sequential color schemes
+    elif color_scheme == 'Viridis':
+        colors = px.colors.sequential.Viridis
+    elif color_scheme == 'Plasma':
+        colors = px.colors.sequential.Plasma
+    elif color_scheme == 'Rainbow':
+        colors = px.colors.sequential.Rainbow
+    elif color_scheme == 'Turbo':
+        colors = px.colors.sequential.Turbo
+    elif color_scheme == 'D3':
+        colors = px.colors.qualitative.D3
     return colors[::max(1, len(colors)//n_files)][:n_files]
 
 
