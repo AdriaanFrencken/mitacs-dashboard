6bdca59583e52f25eb8888dec29334655eadf8f8
John Feng <johnfengphd@gmail.com>
2025-03-24 20:30:59 -0700

    Add I-t Curve Analysis App and update app.py navigation
    - Introduce a new Streamlit app (I-t_app.py) for analyzing I-t curves from uploaded CSV files.
    - Update app.py to include the new I-t Curve Analysis page, enhancing user navigation.
    - Implement user controls for pulse alignment, threshold input, and plot customization options.
    - Improve visualization with dynamic color schemes and enhanced layout for better data representation.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-pages = [st.Page('app_v1.py', title = 'I-t Photocurrent Plots'),
+pages = [st.Page('I-t_app.py', title = 'I-t Photocurrent Plots'),
          st.Page('app_leakage_current.py', title = 'I-t Leakage Current Analysis'),
          st.Page('IV_app.py', title = 'I-V Curve Plots'),
          st.Page('IV_power_law.py', title = 'I-V Power Law Analysis')]
