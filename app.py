8d94797e816f65557b1ce7dd43e5e2ab72d3aca8
John Feng <johnfengphd@gmail.com>
2025-03-25 16:58:04 -0700

    Rename file
    - Introduce a new Streamlit app (I-t_leakage_current.py) for analyzing leakage current data with customizable parameters.
    - Update app.py to correct the page reference for the new leakage current analysis app, enhancing user navigation.
    - Implement user controls for threshold current, time range, and visualization options to improve data analysis experience.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pages = [st.Page('I-t_app.py', title = '📈 I-t Photocurrent Plots'),
-         st.Page('app_leakage_current.py', title = '🔍 I-t Leakage Current Analysis'),
+         st.Page('I-t_leakage_current.py', title = '🔍 I-t Leakage Current Analysis'),
          st.Page('IV_app.py', title = '📊 I-V Curve Plots'),
          st.Page('IV_power_law.py', title = '⚡ I-V Power Law Analysis')]
 
